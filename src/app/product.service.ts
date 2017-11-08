a0847cfd7c649389844d561747c70fa0ff3367a3
jaketrent <trent.jake@gmail.com>
2017-11-08 16:20:43 -0700

    part4 - but local tests fail, seem like should pass

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,10 @@
 import 'rxjs/add/operator/map'
+
+import { Observable } from 'rxjs/Observable'
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http'
+
+import { Album } from './album'
 
 @Injectable()
 export class ProductService {
@@ -8,12 +12,12 @@ export class ProductService {
   private _albumUrl = '../assets/album.json'
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(res => res.json())
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map(res => <Album>res.json())
   }
   // TODO: this is required for part3 tests, but not for the solution to work
-  getProducts(id: number) {
-    return this._http.get(this._albumUrl).map(res => res.json())
+  getProducts(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map(res => <Album>res.json())
   }
 
 }
